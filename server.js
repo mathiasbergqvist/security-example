55f53da86c38b7dd3bc1a3e3187023c5aaeba1fa
Mathias <mathiasbergqvist88@gmail.com>
2023-04-14 14:05:10 +0200

    Authentication endpoints with middleware

--- server.js
+++ server.js
@@ -3,16 +3,32 @@ const https = require("https");
 const fs = require("fs");
 const cors = require("cors");
 const helmet = require("helmet");
-const PORT = 6060;
+const path = require("path");
+const morgan = require("morgan");
+const PORT = 6002;
 
 const app = express();
 app.use(helmet());
 app.use(
   cors({
-    origin: "http://localhost:6000",
+    origin: "http://localhost:6002",
     optionsSuccessStatus: 200,
   })
 );
+app.use(morgan("combined")); //Use "dev" for dev output
+
+app.use(express.json());
+
+const checkLoggedIn = (req, res, next) => {
+  const isLoggedIn = true; // TODO
+  if (!isLoggedIn) {
+    return res.status(401).json({
+      error: "You must log in",
+    });
+  }
+  next();
+};
+
 const server = https.createServer(
   {
     key: fs.readFileSync("key.pem"),
@@ -21,8 +37,18 @@ const server = https.createServer(
   app
 );
 
-app.get("/secret", (req, res) => {
+app.get("/auth/google", (req, res) => {});
+
+app.get("/auth/google/callback", (req, res) => {});
+
+app.get("/auth/logout", (req, res) => {});
+
+app.get("/secret", checkLoggedIn, (req, res) => {
   return res.send("The secret value is 42!");
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 server.listen(PORT, () => {
